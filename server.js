e3fe992b1f3738a29b0adc8dde58e027bd8a0a2d
BolatDanial <bolatdanial0103@gmail.com>
2025-02-26 10:07:38 +0500

    Pre-Deploy

--- server.js
+++ server.js
@@ -80,4 +80,7 @@ app.use('/books', bookRoutes);
 app.use('/borrow', borrowRoutes);
 app.use('/admin', adminRoutes)
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
